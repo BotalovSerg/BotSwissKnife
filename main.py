dad4d7ffdcb8d19a55af85523235e6b8f73a119a
Boralov Serg <89090168690@mail.ru>
2023-09-05 15:16:36 +0500

    add[DEV]: update button MENU

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.types import BotCommand
 from config_data.config import load_config, Config
 from handlers import user_handlers
 
@@ -13,10 +14,19 @@ async def main_bot() -> None:
     dp: Dispatcher = Dispatcher()
 
     dp.include_router(user_handlers.router)
+    dp.startup.register(set_main_menu)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
+
+async def set_main_menu(bot: Bot):
+
+    main_menu_commands = [
+        BotCommand(command='/help',
+                   description='Спарвка по работе бота')
+    ]
+    await bot.set_my_commands(main_menu_commands)
 
 if __name__ == '__main__':
     asyncio.run(main_bot())
