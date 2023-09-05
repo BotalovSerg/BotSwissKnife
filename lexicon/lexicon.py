ed7b9b13ef09e00a5d09365ff2b9adaf04a7b3ee
Boralov Serg <89090168690@mail.ru>
2023-09-05 15:33:40 +0500

    add[DEV]: update#2 button MENU

--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -1,3 +1,9 @@
 LEXICON_RU: dict[str, str] = {
     '/start': 'Привет!\n\nЯ бот для демонстрации!\n\n'
 }
+
+
+LEXICON_COMMANDS_RU: dict[str, str] = {
+    '/help': 'Спарвка по работе бота',
+    '/support': 'Поддержка',
+}
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import BotCommand
 from config_data.config import load_config, Config
 from handlers import user_handlers
+from keyboards.set_menu import set_main_menu
 
 
 async def main_bot() -> None:
@@ -14,19 +14,11 @@ async def main_bot() -> None:
     dp: Dispatcher = Dispatcher()
 
     dp.include_router(user_handlers.router)
-    dp.startup.register(set_main_menu)
 
+    await set_main_menu(bot)
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
-
-async def set_main_menu(bot: Bot):
-
-    main_menu_commands = [
-        BotCommand(command='/help',
-                   description='Спарвка по работе бота')
-    ]
-    await bot.set_my_commands(main_menu_commands)
 
 if __name__ == '__main__':
     asyncio.run(main_bot())
