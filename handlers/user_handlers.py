3869b0fefaae3b4523464bfef61477780fb60dba
Boralov Serg <89090168690@mail.ru>
2023-09-26 14:30:49 +0500

    [DEV] Разделил hendlers, добавил инлаин тест меню

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,36 +1,24 @@
 from aiogram import Router
+from aiogram import F
+from aiogram.types import CallbackQuery
 from aiogram.types import Message
-from aiogram.filters import Command, CommandStart
-from lexicon.lexicon import LEXICON_RU, LEXICON_WEATHER_KB
-from keyboards.inline_weather_kb import create_inline_kb
+from aiogram.filters import Command
+from lexicon.lexicon import KEYBOARD_MENU
+from keyboards.inline_create_kb import create_inline_kb
 
 router: Router = Router()
 
 
-@router.message(CommandStart())
-async def cd_start(message: Message):
-    await message.answer(text=LEXICON_RU['/start'])
+@router.message(Command("test"))
+async def cmd_test(message: Message):
+    kb = create_inline_kb(2, **KEYBOARD_MENU)
+    await message.answer(
+        text="📍 Main menu",
+        reply_markup=kb
+    )
 
 
-@router.message(Command("help"))
-async def cmd_help(message: Message):
-    await message.answer(text=LEXICON_RU['/help'])
-
-
-@router.message(Command("support"))
-async def cmd_help(message: Message):
-    await message.answer(text=LEXICON_RU['/support'])
-
-
-@router.message(Command("id_user"))
-async def cmd_user_id(message: Message):
-    await message.answer(text=f"Your ID: {message.from_user.id}")
-
-
-# @router.message(Command("test"))
-# async def cmd_test(message: Message):
-#     kb = create_inline_kb(**LEXICON_WEATHER_KB)
-#     await message.answer(
-#         text='Test kb weather',
-#         reply_markup=kb
-#     )
+@router.callback_query(F.data == "id_user")
+async def cmd_user_id(callback: CallbackQuery):
+    await callback.message.edit_text(f"Your ID: {callback.message.from_user.id}")
+    await callback.answer()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher
 from config_data.config import load_config, Config
-from handlers import user_handlers, weather_handlers
+from handlers import user_handlers, weather_handlers, cmd_handlers
 from keyboards.set_menu import set_main_menu
 
 
@@ -19,6 +19,7 @@ async def main_bot() -> None:
 
     dp.include_router(user_handlers.router)
     dp.include_router(weather_handlers.router)
+    dp.include_router(cmd_handlers.router)
 
     await set_main_menu(bot)
     await bot.delete_webhook(drop_pending_updates=True)
